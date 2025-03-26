04b12a2d20367b895a34e777d5aa7ee0b241f339
MontasirOpi <montasiropi@gmail.com>
2025-03-27 03:02:43 +0600

    bloc_starting

---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,36 @@
+class GroceryData {
+  List<Map<String, dynamic>> products = [
+    {
+      'id': 1,
+      'name': 'Tea',
+      'description': 'A refreshing hot beverage made from tea leaves.',
+      'price': 10.0,
+      'imageUrl':
+          'https://th.bing.com/th/id/R.e8a6f8de33ef09d981fe41d4f4129736?rik=S%2bIr82%2bLlZOGAA&pid=ImgRaw&r=0',
+    },
+    {
+      'id': 2,
+      'name': 'Coffee',
+      'description': 'A strong and energizing drink made from coffee beans.',
+      'price': 15.0,
+      'imageUrl':
+          'https://th.bing.com/th/id/OIP.sxphRAolK5Q42_BPVPpH7AHaE8?w=266&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7',
+    },
+    {
+      'id': 3,
+      'name': 'Juice',
+      'description': 'A healthy and fresh fruit juice with natural sweetness.',
+      'price': 20.0,
+      'imageUrl':
+          'https://images.unsplash.com/photo-1549128247-37e905ebdb3f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+    },
+    {
+      'id': 4,
+      'name': 'Milk',
+      'description': 'Pure and fresh milk rich in calcium and vitamins.',
+      'price': 12.5,
+      'imageUrl':
+          'https://s-i.huffpost.com/gen/1221898/images/o-MILK-facebook.jpg',
+    },
+  ];
+}
