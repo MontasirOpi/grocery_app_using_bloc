1ca257da9f7df528dbc5d851f0e7dd70e151f7ea
MontasirOpi <montasiropi@gmail.com>
2025-03-27 15:44:46 +0600

    bloc logic

--- lib/features/home/bloc/home_bloc_event.dart
+++ lib/features/home/bloc/home_bloc_event.dart
@@ -2,3 +2,17 @@ part of 'home_bloc_bloc.dart';
 
 @immutable
 sealed class HomeBlocEvent {}
+
+class HomeProductWishListButtonClickEvent extends HomeBlocEvent {
+  
+}
+class HomeProductCartButtonClickEvent extends HomeBlocEvent {
+  
+}
+
+class HomeWishlistButtonNavigateEvent extends HomeBlocEvent {
+  
+}
+class HomeCartButtonNavigateEvent extends HomeBlocEvent {
+  
+}
--- lib/features/home/bloc/home_bloc_state.dart
+++ lib/features/home/bloc/home_bloc_state.dart
@@ -2,5 +2,16 @@ part of 'home_bloc_bloc.dart';
 
 @immutable
 sealed class HomeBlocState {}
+abstract class HomeActionState extends HomeBlocState {}
 
 final class HomeBlocInitial extends HomeBlocState {}
+
+class HomeLoadingState extends HomeBlocState {}
+
+class HomeLoadingSuccessState extends HomeBlocState {}
+
+class HomeErrorState extends HomeBlocState {}
+
+class HomeNavigateToWishlistPageActionState extends HomeActionState {}
+
+class HomeNavigateToCartPageActionState extends HomeActionState {}
